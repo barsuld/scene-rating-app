1347666725d183aaf2937db9f727c174f71b95f1
barsuld <barsuld@gmail.com>
2026-02-15 12:47:00 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,10 @@
-const CACHE = "scene-score-v1";
-const ASSETS = ["./", "./index.html"];
+const CACHE = "scenescore-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
